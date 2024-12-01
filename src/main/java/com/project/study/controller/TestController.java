6f4c56cb6ecc7cf4b2ce0a77ec3cfd29d1c89f59
minkyeong <ming920331@gmail.com>
2024-12-01 17:26:32 +0900

    Initial commit

---
+++ src/main/java/com/project/study/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.project.study.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> test(){
+
+        return ResponseEntity.ok("Hello");
+    }
+}
